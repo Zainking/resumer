804febf87595361a249c6934a0e74ee81e01885e
zainking <zainking@foxmail.com>
2017-10-29 19:57:01 +0800

    update

--- task7/src/components/ResumeEditor.vue
+++ task7/src/components/ResumeEditor.vue
@@ -2,19 +2,48 @@
 div.ResumeEditor
   aside
     ul
-      li(v-for="item in resume.visibleItems"  v-bind:class='{ active: item === selected}' @click="selected = item") {{item}}
+      li(v-for="item in resume.itemsConfig"  v-bind:class='{ active: item.name === selected}' @click="selected = item.name")
+        svg
+          use(v-bind:xlink:href="'#icon-' + item.icon")
   div.pannel
     ol
-      li(v-for="item in resume.visibleItems" v-show="item === selected"){{item}}
+      li(v-for="item in resume.itemsConfig" v-show="item.name === selected"){{ resume[item.name] }}
 </template>
 
 <script>
 export default {
   name: 'ResumeEditor',
   data: () => ({
-    selected: 'bio',
+    selected: 'profile',
     resume: {
-      visibleItems: ['bio', 'workhistory', 'education', 'projects', 'awards', 'contacts', 'others']
+      itemsConfig: [{
+        name: 'profile',
+        icon: 'id'
+      }, {
+        name: 'workhistory',
+        icon: 'work'
+      }, {
+        name: 'education',
+        icon: 'book'
+      }, {
+        name: 'projects',
+        icon: 'heart'
+      }, {
+        name: 'awards',
+        icon: 'cup'
+      }, {
+        name: 'contacts',
+        icon: 'phone'
+      }],
+      profile: {
+        name: '',
+        city: ''
+      },
+      workhistory: [],
+      education: [],
+      projects: [],
+      awards: [],
+      contacts: []
     }
   })
 }
@@ -36,12 +65,20 @@ export default {
       margin-top: 20px;
       li
         height: 48px;
-        line-height: 48px;
         width: 80px;
-        text-align:center;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        margin: 16px 0;
+        svg
+          width: 30px;
+          height: 25px;
+          fill: #fff;
       li.active
         background-color: #fff;
         color: #000;
+        svg
+          fill: #000;
   .pannel
     overflow: auto;
     ol
