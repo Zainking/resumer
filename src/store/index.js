2e285ab8d165e90b083f34f10d2194f5e5773b79
Zain King <zainking2016@hotmail.com>
2017-11-02 17:08:32 +0800

    update

--- src/store/index.js
+++ src/store/index.js
@@ -6,7 +6,80 @@ Vue.use(Vuex)
 
 const store = new Vuex.Store({
   state: {
-    currentUser: api.currentUser
+    currentUser: api.currentUser,
+    resumeTemplate: [{
+      name: 'profile',
+      icon: 'id',
+      isSingle: true,
+      title: '个人信息',
+      template: [
+        {item: '姓名', type: 'text'},
+        {item: '年龄', type: 'number'},
+        {item: '所在地', type: 'text'}
+      ]
+    }, {
+      name: 'workhistory',
+      icon: 'work',
+      isSingle: false,
+      title: '工作经历',
+      template: [
+        {item: '工作地点', type: 'text'},
+        {item: '开始时间', type: 'date'},
+        {item: '结束时间', type: 'date'},
+        {item: '工作内容', type: 'text'}
+      ]
+    }, {
+      name: 'education',
+      icon: 'book',
+      isSingle: false,
+      title: '教育经历',
+      template: [
+        {item: '学校', type: 'text'},
+        {item: '学历', type: 'text'},
+        {item: '开始时间', type: 'date'},
+        {item: '结束时间', type: 'date'}
+      ]
+    }, {
+      name: 'projects',
+      icon: 'heart',
+      isSingle: false,
+      title: '项目经验',
+      template: [
+        {item: '项目名称', type: 'text'},
+        {item: '项目介绍', type: 'text'},
+        {item: '负责内容', type: 'text'}
+      ]
+    }, {
+      name: 'awards',
+      icon: 'cup',
+      isSingle: false,
+      title: '获奖经历',
+      template: [
+        {item: '奖项名称', type: 'text'},
+        {item: '奖项介绍', type: 'text'}
+      ]
+    }, {
+      name: 'contacts',
+      icon: 'phone',
+      isSingle: true,
+      title: '联系方式',
+      template: [
+        {item: '手机', type: 'text'},
+        {item: 'Email', type: 'text'},
+        {item: 'QQ', type: 'text'},
+        {item: '微信', type: 'text'}
+      ]
+    }
+  ],
+    resume: {
+      profile: [
+        ['张全蛋', '18', '长沙']
+      ],
+      awards: [
+        ['宇宙装逼一等奖', '装逼得来的'],
+        ['宇宙装逼一等奖', '装逼得来的']
+      ]
+    }
   },
   mutations: {
     login (state, payload) {
